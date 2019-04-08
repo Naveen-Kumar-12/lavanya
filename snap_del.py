531bf696ad010ee9713da27e607dda32d67d4a14
Lavanya Kumar <45307373+Naveen-Kumar-12@users.noreply.github.com>
2019-04-08 06:12:08 +0530

    my method_1.py

---
+++ snap_del.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta, timezone
+
+import boto3
+ec2 = boto3.resource('ec2')
+
+# List(ec2.Snapshot)
+snapshots = ec2.snapshots.filter(OwnerIds=['self'])
+
+
+
+for  snapshot in snapshots:
+    start_time = snapshot.start_time
+    delete_time = datetime.now(tz=timezone.utc) - timedelta(days=0)
+    if start_time<delete_time:
+        snapshot.delete()
+
+        print('Snapshot with Id = {} is deleted '.format(snapshot.snapshot_id))
